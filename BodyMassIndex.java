8cae2a2cdea580ea26f5714f22b4939cbd1f29a5
Ferdi Can <fcan5414@gmail.com>
2023-06-22 20:55:38 +0300

    BoddyMassIndex.java finished

---
+++ BodyMassIndex.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double weight, height, bmi;
+
+        System.out.print("Kilonuzu giriniz: ");
+        weight = input.nextDouble();
+
+        System.out.print("Boyunuzu giriniz(m): ");
+        height = input.nextDouble();
+
+        bmi = weight / (height * height);
+
+        System.out.println("Kilonuz: " + weight + "kg");
+        System.out.println("Boyunuz: " + height + "m");
+        System.out.println("Beden kitle indeksiniz: " + bmi);
+
+    }
+}
