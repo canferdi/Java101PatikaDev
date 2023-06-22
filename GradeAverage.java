25d8a1940e3504a54f40f43e199d0feba9ecd9ee
Ferdi Can <fcan5414@gmail.com>
2023-06-22 15:41:42 +0300

    GradeAverage.java finished

---
+++ GradeAverage.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GradeAveragee {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        int math, physic, chemistry, eng, history, music;
+
+        System.out.print("Please enter your Math greade: ");
+        math = input.nextInt();
+
+        System.out.print("Please enter your Physic greade: ");
+        physic = input.nextInt();
+
+        System.out.print("Please enter your Chemistry greade: ");
+        chemistry = input.nextInt();
+
+        System.out.print("Please enter your English greade: ");
+        eng = input.nextInt();
+
+        System.out.print("Please enter your History greade: ");
+        history = input.nextInt();
+
+        System.out.print("Please enter your Music greade: ");
+        music = input.nextInt();
+
+        double average = (math + physic + chemistry + eng + history + music) / 6;
+
+        String result = (average > 60) ? "Passed the class" : "Failed the class";
+
+        System.out.println("Your greade average: " + average);
+        System.out.println(result);
+    }
+}
