2c2f9059d3664869cebae8eb8ce732759de94380
Ferdi Can <fcan5414@gmail.com>
2023-06-22 15:47:28 +0300

    GradeAverage.java  finished

--- GradeAverage.java
+++ GradeAverage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GradeAveragee {
+public class GradeAverage {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
