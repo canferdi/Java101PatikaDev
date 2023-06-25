f66dfde77352a8d9353dd2f97a5ab583700fedf2
Ferdi Can <fcan5414@gmail.com>
2023-06-25 16:10:05 +0300

    Bugs fixed

--- Mod3Mod4.java
+++ Mod3Mod4.java
@@ -4,21 +4,21 @@ public class Mod3Mod4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int number;
+        int number, total = 0, n = 0;
 
         System.out.print("Lütfen sayı giriniz: ");
         number = input.nextInt();
 
-        System.out.println("3'e ve 4'e tam bölünebilen sayılar: ");
         for (int i = 0; i <= number; i++) {
 
             if (i % 3 == 0 && i % 4 == 0) {
-                System.out.println(i);
+                total += i;
+                n++;
 
             }
-
         }
-
+        System.out.println("3'e ve 4'e tam bölünebilen sayıların ortalaması: " + (double) total / n);
+        input.close();
     }
 
 }
